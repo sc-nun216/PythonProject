92edf788dcc83d73e827b7bd9cd02cf4788d4a55
sc-nun216 <sc.oventure@gmail.com>
2025-06-08 13:57:16 +0800

    Update Project10 Digital Clock Program.py

--- Project10 Digital Clock Program.py
+++ Project10 Digital Clock Program.py
@@ -27,7 +27,7 @@ class DigitalClock(QWidget):  # QWidget = base class
         self.setStyleSheet("background-color: black;")
 
         font_id = QFontDatabase.addApplicationFont("DS-DIGIT.TTF")
-        font_family = QFontDatabase.applicationFontFamilies(font_id)[0] #retrieve first element of font family
+        font_family = QFontDatabase.applicationFontFamilies(font_id)[0] 
         my_font = QFont(font_family, 150)
         self.time_label.setFont((my_font))
 
