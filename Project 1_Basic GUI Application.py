c5923a16ae98b64233dc85bda89757f6c1c71a6a
sc-nun216 <sc.oventure@gmail.com>
2025-06-08 13:55:10 +0800

    Update Project 1_Basic GUI Application.py

--- Project 1_Basic GUI Application.py
+++ Project 1_Basic GUI Application.py
@@ -4,9 +4,9 @@ from PyQt5.QtGui import QFont
 from PyQt5.QtCore import Qt
 
 class MainWindow(QMainWindow):
-    def __init__(self):         # constructor
-        super().__init__()      # to pass arg to parent use superclass
-        self.setGeometry(1300, 850, 900, 800) # x, y coordinates on window, width, height of window
+    def __init__(self):         
+        super().__init__()     
+        self.setGeometry(1300, 850, 900, 800) 
 
         label = QLabel("Hello", self)
         label.setFont(QFont("Sans Serif", 20))
@@ -17,24 +17,11 @@ class MainWindow(QMainWindow):
                             "font-style: italic;"
                             "text-decoration: underline;")
 
-        #label.setAlignment(Qt.AlignTop) # vertically top
-        #label.setAlignment(Qt.AlignBottom)  # vertically Bottom
-        #label.setAlignment(Qt.AlignVCenter)  # vertically center
-
-        #label.setAlignment(Qt.AlignRight)  # horizontally right
-        #label.setAlignment(Qt.AlignHCenter)  # horizontally center
-        #label.setAlignment(Qt.AlignLeft)  # horizontally left
-
-        #label.setAlignment(Qt.AlignHCenter | Qt.AlignTop)  # center & top
-        #label.setAlignment(Qt.AlignHCenter | Qt.AlignBottom)  # center & bottom
-        # label.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)  # | or operator Center
-        #shortcut for center alignment
         label.setAlignment(Qt.AlignCenter)
-
 
 def main():
     app = QApplication(sys.argv)
-    window = MainWindow()  #default behavior is to hide it
+    window = MainWindow()  
     window.show()
     sys.exit(app.exec_())
 
