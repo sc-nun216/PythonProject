ee46d7b2dca0301a962aa3b20243e8d26b569fcd
sc-nun216 <sc.oventure@gmail.com>
2025-06-08 13:54:13 +0800

    Update API Connect_pokemon.py

--- API Connect_pokemon.py
+++ API Connect_pokemon.py
@@ -8,10 +8,9 @@ base_url = "https://pokeapi.co/api/v2/"
 def get_pokemon_info(name):
     url = f"{base_url}/pokemon/{name}"
     response = requests.get(url)
-    # print(response) # gives HTTP response
 
     if response.status_code == 200:
-        pokemon_data = response.json() # convert to python dict
+        pokemon_data = response.json() 
         return pokemon_data
     else:
         print(f"Failed to retrieve data {response.status_code}")
@@ -20,7 +19,7 @@ pokemon_name = "pikachu"
 pokemon_info = get_pokemon_info(pokemon_name)
 
 if pokemon_info:
-    print(f"Name: {pokemon_info["name"]}")  # access the value using key with []
+    print(f"Name: {pokemon_info["name"]}")  
     print(f"Id: {pokemon_info["id"]}")
     print(f"Height: {pokemon_info["height"]}")
     print(f"Weight: {pokemon_info["weight"]}")
